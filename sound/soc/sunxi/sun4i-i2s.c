acf045e69a985cf8c9f376b8e91d0ed9fe141a5b
John Watts <contact@jookia.org>
2025-11-10 02:08:23 +1100

    ASoC: sunxi: sun4i-i2s: Don't use clk_set_rate_exclusive
    I used clk_rate_exclusive_put to try and ensure nothing would mess with
    the I2S clock while it was running.
    
    I assumed if the call failed it would release the lock but this wasn't
    true. Adding a call to clk_rate_exclusive_put would fix this, but I've
    realized I don't know enough about the rest of the ASoC code to know
    where else I would need to release the clock.
    
    So just use clk_set_rate like all the other drivers.
    
    Signed-off-by: John Watts <contact@jookia.org>

--- sound/soc/sunxi/sun4i-i2s.c
+++ sound/soc/sunxi/sun4i-i2s.c
@@ -1008,14 +1008,13 @@ static int sun4i_i2s_set_sysclk(struct snd_soc_dai *dai, int clk_id,
 	}
 
 	if (freq == 0 && i2s->mclk_freq != 0) {
-		clk_rate_exclusive_put(i2s->mod_clk);
 		i2s->mclk_freq = 0;
 		return 0;
 	}
 
 	clk_rate = freq * clk_mul;
 
-	ret = clk_set_rate_exclusive(i2s->mod_clk, clk_rate);
+	ret = clk_set_rate(i2s->mod_clk, clk_rate);
 	if (ret) {
 		dev_err(dai->dev, "Unable to set %pC clock to %i: %d\n",
 			i2s->mod_clk, clk_rate, ret);
